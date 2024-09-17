52d295f9ad93f8296ab9adc502611c448119df76
Maia <matteusmaia889@gmail.com>
2024-09-17 00:47:57 -0300

    feat: Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,21 +1,40 @@
+import { BotMessageSquare, PencilLine, SearchIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
 export default function Sidebar() {
   return (
     <div className='bg-white p-5 text-white'>
-      <ul className='gap-5 flex lg-flex-col '>
+      <ul className='gap-5 flex lg:flex-col '>
         <li className='flex-1 '>
           <Link href="/create-chatbot" className='hover:opacity-50 flex flex-c0
-          ol text-center lg:text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]'>Link</Link>
+          ol text-center lg:text-left lg:flex-row items-center justify-center gap-2 p-5 rounded-md bg-[#2991EE]'>
+            <BotMessageSquare className='h-6 w-6 lg:h-8 lg:w-8'/>
+            <div className='hidden md:inline'>
+              <p className='text-xl'>Create</p>
+              <p className='text-sm font-extralight'>New Chatbot</p>
+            </div>
+          </Link>
         </li>
         <li className='flex-1 '>
           <Link href="/view-chatbots" className='hover:opacity-50 flex flex-c0
-          ol text-center lg:text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]'>Link</Link>
+          ol text-center lg:text-left lg:flex-row items-center justify-center gap-2 p-5 rounded-md bg-[#2991EE]'>
+            <PencilLine className='h-6 w-6 lg:h-8 lg:w-8'/>
+            <div className='hidden md:inline'>
+              <p className='text-xl'>Edit</p>
+              <p className='text-sm font-extralight'>Chatbots</p>
+            </div>
+          </Link>
         </li>
         <li className='flex-1 '>
           <Link href="/review-sessions" className='hover:opacity-50 flex flex-c0
-          ol text-center lg:text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]'>Link</Link>
+          ol text-center lg:text-left lg:flex-row items-center justify-center gap-2 p-5 rounded-md bg-[#2991EE]'>
+            <SearchIcon className='h-6 w-6 lg:h-8 lg:w-8'/>
+            <div className='hidden md:inline'>
+              <p className='text-xl'>View</p>
+              <p className='text-sm font-extralight'>Sessions</p>
+            </div>
+          </Link>
         </li>
       </ul>
     </div>
