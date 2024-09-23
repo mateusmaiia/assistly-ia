467e8c8f2eb3e59c476117c33e94fdd4e75cdf54
Maia <matteusmaia889@gmail.com>
2024-09-23 13:01:28 -0300

    feat: create-chatbot page

--- app/(admin)/create-chatbot/page.tsx
+++ app/(admin)/create-chatbot/page.tsx
@@ -1,9 +1,47 @@
+'use client';
+
 import Avatar from '@/components/Avatar'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import React from 'react'
+import { CREATE_CHATBOT } from '@/graphql/mutations/mutations'
+import { useMutation } from '@apollo/client'
+import { useUser } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation';
+import React, { FormEvent, useState } from 'react'
 
 export default function CreateChatbot() {
+  const {user} = useUser()
+  const [name, setName] = useState("") 
+  const router = useRouter()
+
+  const [createChatbot, {data, loading, error}] = useMutation(
+    CREATE_CHATBOT, 
+    {
+      variables: {
+        clerk_user_id: user?.id,
+        name
+      },
+    }
+  )
+
+  async function handleSubmit(e: FormEvent ){
+    e.preventDefault()
+
+    try {
+      const data = await createChatbot()
+
+      setName("")
+
+      router.push(`/edit-chatbot/${data.data.insertChatbots.id}`)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  if(!user){
+    return null
+  }
+
   return (
     <div className='flex flex-col items-center justify-center md:flex-row md:space-x-10 bg-white p-10 rounded-md m-10'>
       <Avatar seed='create-chatbot' />
@@ -12,18 +50,20 @@ export default function CreateChatbot() {
           <h2 className='font-light'>
             Create a new chatbot to assist you in conversations with your customers.
           </h2>
-          <form className='flex flex-col md:flex-row gap-5 mt-5  '>
+          <form className='flex flex-col md:flex-row gap-5 mt-5' onSubmit={handleSubmit}>
             <Input 
               type='text'
               className='max-w-lg'
+              onChange={(e) => setName(e.target.value)}
               placeholder='Chatbot name...'
               required
               />
 
-            <Button>
-              Create Chatbot
+            <Button type='submit' disabled={loading || !name}>
+              {loading ? "Craeting Chatbot..." : "Create Chatbot"}
             </Button>
         </form>
+        <p className='text-gray-300 mt-5'>Example: Customer Support Chatbot</p>
       </div>
     </div>
   )
