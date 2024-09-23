95ed52ed7dbccaa548987077999d038971a09c8d
Maia <matteusmaia889@gmail.com>
2024-09-23 08:31:15 -0300

    route: api/graphql

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,15 +1,49 @@
-import { NextResponse } from "next/server";
+import { serverClient } from "@/lib/server/serverClient";
+import { gql } from "@apollo/client";
+// import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request : NextResponse){
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PATCH",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+}
+
+export async function POST(request : NextRequest){
   const {query, variables } = await request.json()
 
   try {
+    let  result
+
     if(query.trim().startsWith("mutation")){
       // handle mutations
+      result = await serverClient.mutate({
+        mutation: gql`
+          ${query}
+        `,
+        variables,
+      })
     }else{
       // handle queries
+      result = await serverClient.query({
+        query: gql`
+          ${query}
+        `,
+        variables,
+      })
     }
+
+    const data = result.data
+    // console.log("DATA >>> ", data)
+
+    return NextResponse.json({
+      data
+    },{
+      headers: corsHeaders
+    })
   } catch (error) {
-    
+    return NextResponse.json(error,{
+      status: 500
+    })   
   }
 }
